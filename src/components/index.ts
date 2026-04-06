381d87972016bb2c2c76cf9da18299fc718b8771
fide-developer <faadjarfirdaus@gmail.com>
2026-04-06 14:29:52 +0700

    feat: add barrel import on components

---
+++ src/components/index.ts
@@ -0,0 +1,24 @@
+export { Button } from './Button';
+export type { ButtonProps } from './Button';
+
+export { Link } from './Link';
+export type { LinkProps } from './Link';
+
+export { Typography } from './Typography';
+export type { TypographyProps } from './Typography';
+
+export { Input } from './Input';
+export type { InputProps } from './Input';
+
+export { Radio, RadioButton, RadioGroup } from './RadioButton';
+export type { RadioButtonProps, RadioGroupProps } from './RadioButton';
+
+export { InputGroup } from './InputGroup';
+export type { InputGroupProps } from './InputGroup';
+
+export { Card } from './Card';
+export type { CardProps } from './Card';
+
+export { Logo } from './Logo';
+export { Navbar } from './Navbar';
+export { Footer } from './Footer';
